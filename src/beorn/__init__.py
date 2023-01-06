cdd461b1c20a93d58cce7b55ea36a89783baeb8d
timotheeschaeffer <timo.sch@hotmail.fr>
2023-01-06 17:06:51 +0100

    cleaning stuffs

--- src/beorn/__init__.py
+++ src/beorn/__init__.py
@@ -1,16 +1,9 @@
 from . import *
-from .solver import Source_MAR
 from .param import par
 from .couplings import *
 from .run import *
 from .global_qty import xHII_approx #, tau_CMB
-from .solver_Helium import Source_MAR_Helium
-from .solver_RK45 import Source_MAR_RK45
-from .simple_model import simple_solver
-from .emulator import gen_training_set, gen_Sampling
 from .python_functions import *
-from .solver_profile_H_simple import Source_H_ion_simple
-
 from .simple_model_faster import simple_solver_faster
 
 
